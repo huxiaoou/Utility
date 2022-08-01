238b310af0e1614e2c12b938d4920e3c8dcd3f9d
huxiaoou <516984451@qq.com>
2022-08-01 11:28:10 +0800

    Add set_table_cell_bg_color

--- gen_text_report.V4.py
+++ gen_text_report.V4.py
@@ -11,17 +11,6 @@ import sys
 import pandas as pd
 
 
-def indent_table(t_table: docx.oxml.table, t_indent: int):
-    # noinspection PyProtectedMember
-    tbl_pr = t_table._element.xpath('w:tblPr')
-    if tbl_pr:
-        e = OxmlElement('w:tblInd')
-        e.set(qn('w:w'), str(t_indent))
-        e.set(qn('w:type'), 'dxa')
-        tbl_pr[0].append(e)
-    return 0
-
-
 def reformat_position_df(t_raw_df: pd.DataFrame) -> pd.DataFrame:
     t_raw_df = t_raw_df.fillna("--")
     t_raw_df["持仓数量"] = t_raw_df["持仓数量"].astype(int)
@@ -70,7 +59,37 @@ def get_equity_desc_text(t_report_date: str, t_report_dir: str, t_header_row: in
     return desc, position_df
 
 
-def add_table(t_doc: Document, t_raw_df: pd.DataFrame, t_table_config: dict, t_table_indent: int = -1000) -> int:
+def rgb_to_hex(t_color_code_rgb: tuple) -> str:
+    hr = str(hex(int(t_color_code_rgb[0])))[-2:]
+    hg = str(hex(int(t_color_code_rgb[1])))[-2:]
+    hb = str(hex(int(t_color_code_rgb[2])))[-2:]
+    return hr + hg + hb
+
+
+# ================================= DOCX =================================
+def indent_table(t_table: docx.oxml.table, t_indent: int):
+    # noinspection PyProtectedMember
+    tbl_pr = t_table._element.xpath('w:tblPr')
+    if tbl_pr:
+        e = OxmlElement('w:tblInd')
+        e.set(qn('w:w'), str(t_indent))
+        e.set(qn('w:type'), 'dxa')
+        tbl_pr[0].append(e)
+    return 0
+
+
+def set_table_cell_bg_color(t_table_cell: docx.oxml.table.CT_Tc, t_color_code_rgb: tuple):
+    """
+    set background shading for Header Rows
+    """
+    tbl_cell_properties = t_table_cell._element.tcPr
+    cl_shading = OxmlElement('w:shd')
+    cl_shading.set(qn('w:fill'), rgb_to_hex(t_color_code_rgb=t_color_code_rgb))  # Hex of Dark Blue Shade {R:0x00, G:0x51, B:0x9E}
+    tbl_cell_properties.append(cl_shading)
+    return 0
+
+
+def add_table_from_df(t_doc: Document, t_raw_df: pd.DataFrame, t_table_config: dict, t_table_indent: int = -1000, t_header_bg_color: tuple = (255, 255, 255)) -> int:
     nrow, ncol = t_raw_df.shape
     table = t_doc.add_table(rows=nrow + 1, cols=ncol, style="Table Grid")
     indent_table(table, t_table_indent)
@@ -81,6 +100,7 @@ def add_table(t_doc: Document, t_raw_df: pd.DataFrame, t_table_config: dict, t_t
         hdr_cells[j].text = t_raw_df.columns[j]
         hdr_cells[j].alignment = WD_PARAGRAPH_ALIGNMENT.LEFT
         hdr_cells[j].width = Cm(t_table_config[t_raw_df.columns[j]]["width"])
+        set_table_cell_bg_color(t_table_cell=hdr_cells[j], t_color_code_rgb=t_header_bg_color)
 
     # update contents
     for i in range(nrow):
@@ -130,7 +150,7 @@ out_put_dir = os.path.join("C:", "Users", "Administrator", "Desktop")
 # out_put_dir = "."
 
 table_config = {
-    "证券名称": {"width": 3.2, },
+    "证券名称": {"width": 3.0, },
     "证券代码": {"width": 1.6, },
     "持仓数量": {"width": 1.6, },
     "单位成本": {"width": 1.7, },
@@ -138,9 +158,10 @@ table_config = {
     "收盘价": {"width": 1.7, },
     "证券市值": {"width": 2, },
     "浮动盈亏": {"width": 2, },
-    "比例": {"width": 1.6, },
+    "比例": {"width": 1.4, },
 }
 norm_font_size = 14
+table_header_bg_color = (195, 195, 195)
 
 # --- CREATE NEW
 report = Document()
@@ -157,7 +178,9 @@ paragraph_count += 1
 change_run_fonts(report.add_paragraph().add_run(commodity_desc_text), "Fangsong", norm_font_size, "仿宋")
 paragraph_count += 1
 report.paragraphs[paragraph_count].paragraph_format.first_line_indent = Pt(norm_font_size * 2)
-add_table(t_doc=report, t_raw_df=commodity_pos_df, t_table_config=table_config)
+add_table_from_df(t_doc=report, t_raw_df=commodity_pos_df, t_table_config=table_config, t_header_bg_color=table_header_bg_color)
+
+report.add_page_break()
 
 # --- EQUITY
 equity_desc_text, equity_pos_df = get_equity_desc_text(t_report_date=report_date, t_report_dir=equity_src_dir)
@@ -166,7 +189,7 @@ paragraph_count += 1
 change_run_fonts(report.add_paragraph().add_run(equity_desc_text), "Fangsong", norm_font_size, "仿宋")
 paragraph_count += 1
 report.paragraphs[paragraph_count].paragraph_format.first_line_indent = Pt(norm_font_size * 2)
-add_table(t_doc=report, t_raw_df=equity_pos_df, t_table_config=table_config)
+add_table_from_df(t_doc=report, t_raw_df=equity_pos_df, t_table_config=table_config, t_header_bg_color=table_header_bg_color)
 
 # --- SAVE
 report_file = "日报_{}.docx".format(report_date)
