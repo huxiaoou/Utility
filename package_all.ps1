a8ce504dc45acb47e8ad3f13b6816273e311e6ba
huxiaoou <516984451@qq.com>
2022-08-01 13:29:43 +0800

    Update package_all.ps1

--- package_all.ps1
+++ package_all.ps1
@@ -7,51 +7,32 @@ if (-not($trade_date)) {
 $trade_year = $trade_date.substring(0, 4)
 $sep = "=" * 120
 
+# for Linchen
 Write-Host $sep
-python gen_text_report.V3.py $trade_date
+python gen_text_report.V4.py $trade_date
 
-# # for zengtongyi
-# Write-Host $sep
-# Copy-Item \Works\Trade\Reports\output\$trade_year\$trade_date\0[3-5]_* .
-# Copy-Item \Works\Trade\Reports_Merge\output\$trade_year\$trade_date\0[3-5]_* .
-# 7z a C:\Users\Administrator\Desktop\$trade_date *.xlsx
-# Remove-Item *.xlsx
-
-# for qiuyue
+# for Qiuyue
 Write-Host $sep
-Copy-Item \Works\Trade\Reports\output\$trade_year\$trade_date\08* .
-Copy-Item \Works\Trade\Reports_Equity2\output\$trade_year\$trade_date\07* .
-7z a C:\Users\Administrator\Desktop\$trade_date-财务-邱玥 *.xlsx
-Remove-Item *.xlsx
+7z a C:\Users\Administrator\Desktop\$trade_date-财务-邱玥 \Works\Trade\Reports\output\$trade_year\$trade_date\08*
+7z a C:\Users\Administrator\Desktop\$trade_date-财务-邱玥 \Works\Trade\Reports_Equity2\output\$trade_year\$trade_date\07*
 
-# # for zhouyishan
-# Write-Host $sep
-# Copy-Item \Works\Trade\Reports\output\$trade_year\$trade_date\0[6]* .
-# Copy-Item \Works\Trade\Reports_Equity2\output\$trade_year\$trade_date\0[6]* .
-# 7z a C:\Users\Administrator\Desktop\$trade_date-周怡杉 *.xlsx
-# Remove-Item *.xlsx
-
+# for Qianyelan
 Write-Host $sep
-# for qianyelan
-Copy-Item \Works\Trade\Reports\output\$trade_year\$trade_date\06* .
-Copy-Item \Works\Trade\Reports_Merge\output\$trade_year\$trade_date\04* .
-7z a C:\Users\Administrator\Desktop\$trade_date-钱叶兰 *.xlsx
-Remove-Item *.xlsx
+7z a C:\Users\Administrator\Desktop\$trade_date-钱叶兰 \Works\Trade\Reports\output\$trade_year\$trade_date\06*
+7z a C:\Users\Administrator\Desktop\$trade_date-钱叶兰 \Works\Trade\Reports_Merge\output\$trade_year\$trade_date\04*
 
+# for Fanyabin
 Write-Host $sep
-# for fanyabin
-Copy-Item \Works\Trade\Reports\output\$trade_year\$trade_date\0[4-6]* .
-Copy-Item \Works\Trade\Reports\intermediary\组合净值.xlsx .
-Copy-Item \Works\Trade\Reports\intermediary\*.png .
-7z a C:\Users\Administrator\Desktop\$trade_date-樊亚彬 *.xlsx
-7z a C:\Users\Administrator\Desktop\$trade_date-樊亚彬 *.png
-Remove-Item *.xlsx
-Remove-Item *.png
+7z a C:\Users\Administrator\Desktop\$trade_date-樊亚彬 \Works\Trade\Reports\output\$trade_year\$trade_date\04*
+7z a C:\Users\Administrator\Desktop\$trade_date-樊亚彬 \Works\Trade\Reports\output\$trade_year\$trade_date\05*
+7z a C:\Users\Administrator\Desktop\$trade_date-樊亚彬 \Works\Trade\Reports\output\$trade_year\$trade_date\06*
+7z a C:\Users\Administrator\Desktop\$trade_date-樊亚彬 \Works\Trade\Reports\intermediary\组合净值.xlsx
+7z a C:\Users\Administrator\Desktop\$trade_date-樊亚彬 \Works\Trade\Reports\intermediary\*.png
 
-Write-Host $sep
-# for daily report
-Copy-Item \Works\Trade\Reports\templates\clean\日报_YYYYMMDD.docx C:\Users\Administrator\Desktop\日报_$trade_date.docx
-Start-Process C:\Users\Administrator\Desktop\日报_$trade_date.docx
-Start-Process \Works\Trade\Reports\output\$trade_year\$trade_date\04_衍生品持仓情况明细表_大宗商品_$trade_date.xlsx
-Start-Process \Works\Trade\Reports_Merge\output\$trade_year\$trade_date\04_持仓情况明细表_固收托管项目_$trade_date.xlsx
+## for daily report
+#Write-Host $sep
+#Copy-Item \Works\Trade\Reports\templates\clean\日报_YYYYMMDD.docx C:\Users\Administrator\Desktop\日报_$trade_date.docx
+#Start-Process C:\Users\Administrator\Desktop\日报_$trade_date.docx
+#Start-Process \Works\Trade\Reports\output\$trade_year\$trade_date\04_衍生品持仓情况明细表_大宗商品_$trade_date.xlsx
+#Start-Process \Works\Trade\Reports_Merge\output\$trade_year\$trade_date\04_持仓情况明细表_固收托管项目_$trade_date.xlsx
 Pause
